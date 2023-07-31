2ba9af0d958f780260102fc01da69945e08b41b4
shochmut <50213094+shochmut@users.noreply.github.com>
2023-07-30 18:49:26 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+import Stack from '@mui/material/Stack';
+
+function App() {
+
+  return (
+    <>
+      <Stack> </Stack>
+    </>
+  )
+}
+
+export default App;
