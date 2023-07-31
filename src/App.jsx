e4b1a730966e6b4e04e6160c2e789d4415d1d2b2
shochmut <50213094+shochmut@users.noreply.github.com>
2023-07-30 19:08:39 -0600

    Initialize buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import { useState } from 'react';
 import './App.css';
 import Stack from '@mui/material/Stack';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import IconButton from '@mui/material/IconButton';
+
 
 function App() {
 
   return (
     <>
-      <Stack> </Stack>
+      <Stack> 
+        <p className="break-label">Break Length</p>
+        <IconButton className="break-decrement">
+          <ArrowDownwardIcon></ArrowDownwardIcon>
+        </IconButton>
+      </Stack>
+      <Stack>
+        <p className="session-label">Session Label</p>
+      </Stack>
     </>
   )
 }
