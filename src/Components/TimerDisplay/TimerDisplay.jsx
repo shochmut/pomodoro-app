eb6fdad467f84bdd3db9033927273f5368f76409
shochmut <50213094+shochmut@users.noreply.github.com>
2023-08-04 18:20:01 -0600

    1 file modified

--- src/Components/TimerDisplay/TimerDisplay.jsx
+++ src/Components/TimerDisplay/TimerDisplay.jsx
@@ -12,16 +12,43 @@ import { useSelector, useDispatch } from 'react-redux';
 import { decrement, increment, incrementByAmount, selectTime } from './timerSlice';
 
 const TimerDisplay = () => {
-  const session = useSelector(selectTime)
-  const dispatch = useDispatch()
-  const [incrementAmount, setIncrementAmount] = useState('2')
+  const session = useSelector(selectTime);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState('2');
+  const [isActive, setIsActive] = useState(false);
+  const numZero = 0;
+  const [seconds, setSeconds] = useState(numZero.toFixed(2));
+  const [minutes, setMinutes] = useState(session);
+
+  const toggle = () => {
+    setIsActive(!isActive);
+  }
+
+  useEffect(() => {
+    let newInterval = setInterval(() => {
+      if (isActive && seconds>0) {
+        setSeconds(seconds - 1);
+      }
+      if (isActive && seconds===0) {
+        if (minutes===0) {
+          clearInterval(newInterval)
+        } else {
+          setMinutes(minutes-1);
+          setSeconds(59);
+          }
+        }
+      }, 1000)
+      return () => {
+        clearInterval(newInterval);
+      };
+  });
 
   return (
     <Stack>
       <p className="timer-label">Session</p>
-      <p className="time-left">{session}</p>
+      <p className="time-left">{minutes}:{seconds}</p>
       <Stack direction="row" justifyContent="center">
-        <IconButton className="start_stop" color="success" onClick={() => dispatch(increment())}>
+        <IconButton className="start_stop" color="success" onClick={() => toggle()}>
           <PlayCircleFilledIcon></PlayCircleFilledIcon>
         </IconButton>
         <IconButton className="pause" color="primary" onClick={() => dispatch(increment())}>
